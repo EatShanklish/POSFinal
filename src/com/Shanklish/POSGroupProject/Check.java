1572d9266d42f9d256d86a3384cd724306c86ad1
Logan Somner <logan.somner@gmail.com>
2016-06-20 16:21:37 -0400

    adds check and credit classes

---
+++ src/com/Shanklish/POSGroupProject/Check.java
@@ -0,0 +1,9 @@
+package com.Shanklish.POSGroupProject;
+
+public class Check extends Pay {
+
+	
+	public static String checkPay(){
+		return "Your check payment has been successfully processed.";
+	}
+}
